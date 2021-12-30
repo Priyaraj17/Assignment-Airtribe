4a6da376ceac1c46a9f68024b6d11e00a16527a9
Priyaraj17 <priyaraj.17@outlook.com>
2021-12-31 01:47:13 +0530

    Removed Unnecessary files

---
+++ HTMLAttributes.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: "h1[itemprop=name]",
+  upvotes: ".js-vote-count",
+  n_ans: "div.no-answers",
+  ans: "span[itemprop=answerCount]",
+  questionURL: "h3>a.question-hyperlink",
+  related: "div>a.question-hyperlink",
+};
